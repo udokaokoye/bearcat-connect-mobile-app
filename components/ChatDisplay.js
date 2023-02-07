9be42d0a204c0666ae22a64b240592493d7c40ac
udoka <leviokoye@gmail.com>
2023-02-07 07:27:54 -0500

    added timestamp to ChatDisplay

--- components/ChatDisplay.js
+++ components/ChatDisplay.js
@@ -1,10 +1,13 @@
 import { View, Text, Image } from 'react-native'
 import React, {useEffect} from 'react'
+import firebase from 'firebase/compat/app';
+import moment from 'moment';
 
 const ChatDisplay = ({chat, sender, sgUser}) => {
-    useEffect(() => {
-    //   console.log(sender)
-    }, [])
+    // useEffect(() => {
+    //   console.log()
+    //   // console.log(moment(chat.timestamp.nanoseconds).format('LT'))
+    // }, [])
     
     // console.log(sender)
   return (
@@ -13,8 +16,11 @@ const ChatDisplay = ({chat, sender, sgUser}) => {
     {!sender ? (
                 <Image source={{uri: chat.profile_picture}} className='rounded-full self-end' style={{width: 30, height: 30}} />
             ) : ''}
-        <View className={`flex-1 ml-2 p-5 ${sender ? 'ml-0 bg-red-600' : 'bg-gray-200'} justify-center items-center rounded-xl`} style={{minHeight: 50}}>
+        <View className='flex-1' style={{minHeight: 50}} >
+          <View className={`flex-1 ml-2 p-5 ${sender ? 'ml-0 bg-red-600' : 'bg-gray-200'} justify-center items-center rounded-xl`} >
         <Text style={{color: sender? 'white' :'black'}}>{chat.message}</Text>
+        </View>
+        <Text className='ml-1 mt-1 text-gray-400'>{moment(chat?.timestamp?.toDate()).format("LT")}</Text>
         </View>
     </View>
     </View>
